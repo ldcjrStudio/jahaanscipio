a26d02baa0dade2840b780e2873d0ac71869e4cd
Leroy Clarke Jr <lclarkejr94@gmail.com>
2021-09-03 22:05:45 -0400

    API call setup

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Head from "next/head";
+import getProjects from "../lib/projects";
 
-export default function Home() {
+export default function Home({ projects }) {
+  console.log(projects);
   return (
     <section className="flex flex-col items-center justify-center min-h-screen py-2">
-      <p>hello</p>
+      {projects.map((project) => (
+        <div key={project.id}>
+          <p>{project.fields.Name}</p>
+        </div>
+      ))}
     </section>
   );
+}
+
+export async function getStaticProps({}) {
+  const projects = await getProjects();
+
+  return {
+    props: { projects },
+  };
 }
